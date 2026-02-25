b9484d8c230f9dd7a71a391083f699e5814d4c46
Rashedulman <rashedulalam2k02@gmail.com>
2026-02-25 11:50:34 +0600

    added call button function

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 // heart icon functionality 
 
+//  function increaseHeart(){
+//     const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+
+//     const totalNewAvailableHeart = availableHeart + 1
+
+//     document.getElementById("available-heart").innerText=totalNewAvailableHeart
+// }
+
+
+
+
+function getElement(id){
+    const element = document.getElementById(id)
+    return element
+}
+
  function increaseHeart(){
-    const availableHeart = parseInt(document.getElementById("available-heart").innerText)
+    const availableHeart = parseInt(getElement("available-heart").innerText)
 
     const totalNewAvailableHeart = availableHeart + 1
 
-    document.getElementById("available-heart").innerText=totalNewAvailableHeart
+    getElement("available-heart").innerText=totalNewAvailableHeart
 }
 
+getElement('service-box').addEventListener('click', function(e){
+    if(e.target.className.includes('call-btn')){
+        // alert('cart button clicked')
+        const callButton = e.target
 
+        const serviceName = callButton.parentNode.parentNode.children[1].children[0].innerText
+        const serviceNumber = callButton.parentNode.parentNode.children[1].children[2].innerText
 
+        const availableCoin = parseInt(getElement("available-coin").innerText)
+         
+        if(availableCoin<20){
+            alert("❌ Insufficient coins. A minimum of 20 coins is required to make a call.")
+            return
+        }
 
+        const NewAvailableCoin = availableCoin - 20 
+        getElement("available-coin").innerText = NewAvailableCoin
+
+         alert("📞 Calling " + serviceName + " " + serviceNumber +" ...")
+
+          const historyContainer = getElement('history-container')
+
+          const newHistory = document.createElement('div')
+          newHistory.innerHTML = `
+          <div class="rounded-xl bg-gray-100 flex justify-between items-center
+                        p-4">
+                          
+                          <div class="">
+                            <h2 class="font-bold">${serviceName}</h2>
+                            <h2 class="">${serviceNumber}</h2>
+                          </div>
+                          <span class="text-xl text-black-400">
+                ${new Date().toLocaleTimeString()}
+            </span>
+                        </div>
+          `
+          historyContainer.append(newHistory)
+
+    }
+})
